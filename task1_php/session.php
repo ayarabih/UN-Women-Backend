d192fdfca183a727eb90e9fed5caee7ccf858300
ayarabih <ayarabih@github.com>
2023-08-20 00:45:42 +0300

    task1 php and questions

---
+++ task1_php/session.php
@@ -0,0 +1,19 @@
+<!-- task 2  -->
+<!-- Write a program to print “Hello World” using echo -->
+<?php
+$data = "Hello world!<br>";
+echo $data;
+?>
+<!-- Write a program to print 2 php variables using single echo statement. -->
+<?php
+$x = 1000; 
+echo $x;
+echo "<br>";
+$z = "sum<br>";
+echo $z;
+?>
+
+
+
+
+
